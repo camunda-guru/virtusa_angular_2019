efc7cb002005e61c033290d4380c4caa6bc8b908
eswaribala <Parameswaribala@gmail.com>
2019-01-03 15:50:16 +0530

    Add files via upload

--- models.menudata.ts
+++ models.menudata.ts
@@ -3,11 +3,14 @@ import {Item, ModelsMenu} from "./models.menu";
 
 export  let data:ModelsMenu[]=[
 
-  new ModelsMenu('Add', 'pi pi-fw pi-plus',[new Item('Revenue Reporting','pi pi-fw pi-share-alt'),
-    new Item('Revenue Editor','pi pi-fw pi-pencil'),
-    new Item('PENL','pi pi-fw pi-cog'),
-    new Item('Trade Authorization','pi pi-fw pi-key'),
-    new Item('Blogging','pi pi-fw pi-comment')
+  new ModelsMenu('Add', 'pi pi-fw pi-plus',[new Item('RevenueReporting',
+
+    'pi pi-fw pi-share-alt',[{ outlets: { RROutlet: ['' +
+        ''] } }]),
+    new Item('RevenueEditor','pi pi-fw pi-pencil',[{ outlets: { REOutlet: ['RevenueEditor'] } }]),
+    new Item('PENL','pi pi-fw pi-cog',[]),
+    new Item('TradeAuthorization','pi pi-fw pi-key',[]),
+    new Item('Blogging','pi pi-fw pi-comment',[])
 
   ]),
   new ModelsMenu('Quit', 'pi pi-fw pi-sign-out',[ ])
