ad286af1ac8513ef095c47423a2a21c88ce7fc2a
eswaribala <Parameswaribala@gmail.com>
2019-01-03 14:35:48 +0530

    Add files via upload

--- models.menu.ts
+++ models.menu.ts
@@ -1,6 +1,27 @@
 
 export class ModelsMenu
 {
+  get icon(): string {
+    return this._icon;
+  }
+
+  set icon(value: string) {
+    this._icon = value;
+  }
+  private _label:string;
+  private _icon:string;
+
+
+  private _items:Item[]
+
+  constructor(plabel:string,picon:string,pitems:Item[])
+  {
+
+    this._label=plabel;
+    this._icon=picon;
+    this._items=pitems;
+  }
+
   get items(): Item[] {
     return this._items;
   }
@@ -15,9 +36,7 @@ export class ModelsMenu
   set label(value: string) {
     this._label = value;
   }
-  private _label:string;
 
-  private _items:Item[]
 
 
 
@@ -30,6 +49,14 @@ export class Item
 {
   private _label:string;
   private _icon:string;
+
+  constructor(plabel,picon)
+  {
+    this._label=plabel;
+    this._icon=picon;
+  }
+
+
   get icon(): string {
     return this._icon;
   }
@@ -52,3 +79,6 @@ export class Item
 
 
 }
+
+
+
--- models.menudata.ts
+++ models.menudata.ts
@@ -3,13 +3,13 @@ import {Item, ModelsMenu} from "./models.menu";
 
 export  let data:ModelsMenu[]=[
 
-  new ModelsMenu('Add', [new Item('Revenue Reporting','pi pi-fw pi-plus'),
-    new Item('Revenue Editor','pi pi-fw pi-trash'),
-    new Item('PENL','pi pi-fw pi-plus'),
-    new Item('Trade Authorization','pi pi-fw pi-plus'),
-    new Item('Blogging','pi pi-fw pi-plus')
+  new ModelsMenu('Add', 'pi pi-fw pi-plus',[new Item('Revenue Reporting','pi pi-fw pi-share-alt'),
+    new Item('Revenue Editor','pi pi-fw pi-pencil'),
+    new Item('PENL','pi pi-fw pi-cog'),
+    new Item('Trade Authorization','pi pi-fw pi-key'),
+    new Item('Blogging','pi pi-fw pi-comment')
 
   ]),
-  new ModelsMenu('Quit', [ ])
+  new ModelsMenu('Quit', 'pi pi-fw pi-sign-out',[ ])
 
 ]
